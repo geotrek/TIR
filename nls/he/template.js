bf0bcac3d49b390417b0fc6aa5981136d0fc97b4
Paul <geotrekgis@gmail.com>
2013-06-11 12:04:38 -0700

    this is amessage

---
+++ nls/he/template.js
@@ -0,0 +1,16 @@
+﻿define(
+({
+  viewer:{
+    main:{
+      title: "השווה Web Maps",
+      subtitle: "השווה Web Maps עם Just A Swipe",
+      loadingMessage: "טוען מפה",
+      scaleBarUnits: "metric" //"english (for miles) or "metric" (for km) - don't translate.
+    },
+    errors:{
+      createMap: "לא ניתן ליצור מפה",
+      general: "שגיאה"
+    }
+  }
+})
+);
